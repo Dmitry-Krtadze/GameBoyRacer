359dba64439403a48d409f52001cf597a0e85562
Dmitry-Krtadze <73993967+Dmitry-Krtadze@users.noreply.github.com>
2026-03-28 12:05:17 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    src/enemy.h
    	modified:   src/main.cpp
    	deleted:    src/player.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,75 +2,45 @@
 #include <GameBoyFixVal.h>
 GameBoy gb;
 
-#include "enemy.h"
-#include "player.h"
-
-int enemy_x = 2;
-int car_speed = 100;
-int mycar_x = 2;
-int mycar_y = 12;
-
-void setup() { 
+int dotX[50];                  //✅
+int dotY[50];                  //✅
+int dotCount = 1;              //✅
+void setup() {  
   gb.begin(0);
-  playerCar(mycar_x, mycar_y);
-  randomSeed(analogRead(0));
+  randomSeed(analogRead(0));   
+  dotX[0] = random(0, 8);      //✅
+  dotY[0] = random(0, 16);     //✅
 }
-void createLine(int y){
-  gb.drawPoint(0,y);
-  gb.drawPoint(0,y + 1);
-  gb.drawPoint(0,y + 2);
-
-  gb.drawPoint(7,y);
-  gb.drawPoint(7,y + 1);
-  gb.drawPoint(7,y + 2);
+int x = 1, y = 0;
+int dirX = 3, dirY = 7; 
+void makeMove(){
+  if(gb.getKey() == 4){  x = -1;  y = 0;  }
+  if(gb.getKey() == 5){  x = 1;   y = 0;  } 
+  if(gb.getKey() == 6){  x = 0;   y = 1;  }
+  if(gb.getKey() == 3){  x = 0;   y = -1; }
 }
+void loop() { 
+  makeMove();
+  dirX = dirX + x;
+  dirY = dirY + y;
+  if (dirX > 7) { dirX = 0; }
+  if (dirX < 0) { dirX = 7; }
+  if (dirY > 15) { dirY = 0; }
+  if (dirY < 0) { dirY = 15; }
 
-void clearLine(int y){
-  gb.wipePoint(0,y);
-  gb.wipePoint(0,y + 1);
-  gb.wipePoint(0,y + 2);
+  for (int i = 0; i < dotCount; i = i + 1) {                  //✅
+    if (dirX == dotX[i] && dirY == dotY[i]) {                 //✅
+      dotX[i] = random(0, 8);                                 //✅
+      dotY[i] = random(0, 16);                                //✅
+      dotX[dotCount] = random(0, 8);                          //✅
+      dotY[dotCount] = random(0, 16);                         //✅
+    }                                                         //✅
+  }                                                           //✅
+  gb.clearDisplay();                                          //✅
 
-  gb.wipePoint(7,y);
-  gb.wipePoint(7,y + 1);
-  gb.wipePoint(7,y + 2);
+  for (int i = 0; i < dotCount; i = i + 1) {                  //✅
+    gb.drawPoint(dotX[i], dotY[i]);                           //✅
+  }                                                           //✅
+  gb.drawPoint(dirX, dirY);                                   //✅
+  delay(300);                                                 //✅
 }
-void mainRaicing(){
-  enemy_x = random(0,10);
-  if (enemy_x > 5){
-    enemy_x = 2;
-  }else{
-    enemy_x =  5;
-  }
-  for(int enemy_y = 0; enemy_y < 16; enemy_y++){
-    createLine(enemy_y); // +
-    createLine(enemy_y + 5); // +
-    createLine(enemy_y + 10); // +
-    createLine(enemy_y + 15); // +
-    createLine(enemy_y - 5); // +
-    createLine(enemy_y - 10); // +
-    createLine(enemy_y - 15); // +
-    enemyCar(enemy_x, enemy_y);
-    if(gb.getKey() == 5){
-      ClearPlayerCar(2,12);
-      playerCar(5,12);
-    }
-    else if(gb.getKey() == 4){
-      ClearPlayerCar(5,12);
-      playerCar(2,12);
-    }
-    delay(car_speed);
-
-    clearLine(enemy_y);
-    clearLine(enemy_y + 5);
-    clearLine(enemy_y + 10);
-    clearLine(enemy_y + 15);
-    clearLine(enemy_y - 5);
-    clearLine(enemy_y - 10);
-    clearLine(enemy_y - 15);
-    ClearEnemyCar(enemy_x, enemy_y);
-  }
-}
-
-
-
-void loop() {   mainRaicing(); }
