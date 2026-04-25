d76dc1e88146d35064e9414717fcbcd8adce6299
Dmitry-Krtadze <73993967+Dmitry-Krtadze@users.noreply.github.com>
2026-04-25 12:05:03 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   src/blocks.h
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,26 @@
 #include <Arduino.h>
-#include <GameBoyFixVal.h>
+#include "GameBoyFixVal.h"
+#include "blocks.h"
 GameBoy gb;
+void setup() {  gb.begin(0); }
 
-int dotX[50];                  
-int dotY[50];                  
-int dotCount = 1;              
-int snakeX[10];       
-int snakeY[10];       
-
-int up = 0;  
-int right = 1;  
-int bottom = 2;  
-int left = 3;    
-int direction = right; 
-
-
-int lenSnake = 5;
-
-void setup() {  
-  gb.begin(0);
-  snakeX[0] = 4;
-  snakeY[0] = 7;
-  randomSeed(analogRead(0));   
-  dotX[0] = random(0, 8);      
-  dotY[0] = random(0, 16);     
-}
-int x = 1, y = 0;
-int dirX = 3, dirY = 7; 
-
-void makeMove(){
-  if(gb.getKey() == 4 && direction != right){  direction = left;  }
-  if(gb.getKey() == 5 && direction != left) {  direction = right;  } 
-  if(gb.getKey() == 6 && direction != up)   {  direction = bottom;  }
-  if(gb.getKey() == 3 && direction != bottom){  direction = up; }
-}
-void loss()
-{
-  delay(2000);
+void loop() {
   gb.clearDisplay();
-  for(int i = 0; i < lenSnake; i++)
-  {
-    snakeX[i] = 0;
-    snakeY[i] = 0;
-  }
-  direction = right;
-  dotX[0] = 3;
-  dotY[0] = 3;
-  lenSnake = 4;
-  snakeX[0] = 4;
-  snakeY[0] = 7;
-}
-
-
-void move(){
-
-  for(int i = lenSnake - 1; i > 0; i--){
-    if(snakeX[0]==snakeX[i] && snakeY[0] == snakeY[i]){
-      loss();
+  for (uint8_t y = 0; y < 8; y++) {
+    for (uint8_t x = 0; x < 8; x++) {
+      if (Z_Block_1[y][x]) {
+        gb.drawPoint(x, y);
+      }
     }
   }
-
-  for (int i = lenSnake -1; i > 0; i--){
-    snakeX[i] = snakeX[i - 1];
-    snakeY[i] = snakeY[i - 1];
-  }
-
-  if (direction == up){
-    if(snakeY[0] == 0){
-      snakeY[0] = 15;
-    }else{
-      snakeY[0]--;
-    }
-  }
-  
-  if (direction == bottom){
-    if(snakeY[0] == 15){
-      snakeY[0] = 0;
-    }else{
-      snakeY[0]++;
-    }
-  }
-  //--------------------------
-  if (direction == left){
-    if(snakeX[0] == 0){
-      snakeX[0] = 7;
-    }else{
-      snakeX[0]--;
-    }
-  }
-  //--------------------------
-  if (direction == right){
-    if(snakeX[0] == 7){
-      snakeX[0] = 0;
-    }else{
-      snakeX[0]++;
-    }
-  }
-  
-}
-
-void drawSnake(){
-  for (int i =0; i < lenSnake; i++){
-    gb.drawPoint(snakeX[i], snakeY[i]);
-  }
-}
-
-
-
-void loop() { 
-  makeMove();
-  move();
+  delay(500);
   gb.clearDisplay();
-  drawSnake();                             
-  delay(250);                                                 
+  for (uint8_t y = 0; y < 8; y++) {
+    for (uint8_t x = 0; x < 8; x++) {
+      if (Z_Block_1[y][x]) {
+        gb.drawPoint(x, y);
+      }
+    }
+  }
+  delay(500);
 }
-//   https://github.com/Dmitry-Krtadze/GameBoyRacer/
